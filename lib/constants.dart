98a6a82be1f8041392a875b74556ab0b3729d184
Rashid Saleem <mrashidcit@gmail.com>
2023-03-10 11:22:01 +0500

    PayPal Integration in Flutter have been completed

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,8 @@
 
 
 class Constants {
-  static const String clientId = '';
-  static const String secretKey = '';
-  static const String returnURL = '';
-  static const String cancelURL = '';
+  static const String clientId = 'AWE4nOcrIJT3zplYkgsIGKpwgnF_mEVrPyT3CDOElUCHTjWWMRVbASRIJ5PiOLpYBDVIAlrEUMnUeJ-o';
+  static const String secretKey = 'ELq-PzFTmyYCTUxAOHWCDz7bNrsacacl5y0EQz_NHosamjh-Q9l3slWqKM7FsfDLODxr0MF3UuHy-mva';
+  static const String returnURL = 'https://samplesite.com/return';
+  static const String cancelURL = 'https://samplesite.com/cancel';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:one_context/one_context.dart';
 import 'package:paypal_integration_app/constants.dart';
 import 'package:flutter_paypal/flutter_paypal.dart';
+import 'package:paypal_integration_app/helpers/ui_helper.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+bool debugShowCheckedModeBanner = false;
+const localeEnglish = [Locale('en', '')];
+
+// void main() {
+//   // runApp(const MyApp());
+//   void main() => OnePlatform.app = () => MyApp();
+// }
+
+void main() => OnePlatform.app = () => MyApp();
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key}) {
+    print('>> MyApp2 loaded!');
+    OneContext().key = GlobalKey<NavigatorState>();
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        
-        primarySwatch: Colors.blue,
+    log('>> MyApp - build()');
+    // Place that widget on most top
+
+    return OneNotification(
+      builder: (_, __) => MaterialApp(
+        title: 'Flutter Demo',
+        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        builder: OneContext().builder,
+        navigatorKey: OneContext().key,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -26,7 +42,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  
   final String title;
 
   @override
@@ -57,76 +72,81 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(onPressed: () {
-              Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => UsePaypal(
-                            sandboxMode: true,
-                            clientId:
-                                "${Constants.clientId}",
-                            secretKey:
-                                "${Constants.secretKey}",
-                            returnURL: "${Constants.returnURL}",
-                            cancelURL: "${Constants.cancelURL}",
-                            transactions: const [
-                              {
-                                "amount": {
-                                  "total": '10.12',
-                                  "currency": "USD",
-                                  "details": {
-                                    "subtotal": '10.12',
-                                    "shipping": '0',
-                                    "shipping_discount": 0
-                                  }
-                                },
-                                "description":
-                                    "The payment transaction description.",
-                                // "payment_options": {
-                                //   "allowed_payment_method":
-                                //       "INSTANT_FUNDING_SOURCE"
-                                // },
-                                "item_list": {
-                                  "items": [
-                                    {
-                                      "name": "A demo product",
-                                      "quantity": 1,
-                                      "price": '10.12',
-                                      "currency": "USD"
-                                    }
-                                  ],
-
-                                  // shipping address is not required though
-                                  "shipping_address": {
-                                    "recipient_name": "Jane Foster",
-                                    "line1": "Travis County",
-                                    "line2": "",
-                                    "city": "Austin",
-                                    "country_code": "US",
-                                    "postal_code": "73301",
-                                    "phone": "+00000000",
-                                    "state": "Texas"
-                                  },
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => UsePaypal(
+                          sandboxMode: true,
+                          clientId: "${Constants.clientId}",
+                          secretKey: "${Constants.secretKey}",
+                          returnURL: "${Constants.returnURL}",
+                          cancelURL: "${Constants.cancelURL}",
+                          transactions: const [
+                            {
+                              "amount": {
+                                "total": '10.12',
+                                "currency": "USD",
+                                "details": {
+                                  "subtotal": '10.12',
+                                  "shipping": '0',
+                                  "shipping_discount": 0
                                 }
+                              },
+                              "description":
+                                  "The payment transaction description.",
+                              // "payment_options": {
+                              //   "allowed_payment_method":
+                              //       "INSTANT_FUNDING_SOURCE"
+                              // },
+                              "item_list": {
+                                "items": [
+                                  {
+                                    "name": "A demo product",
+                                    "quantity": 1,
+                                    "price": '10.12',
+                                    "currency": "USD"
+                                  }
+                                ],
+
+                                // shipping address is not required though
+                                "shipping_address": {
+                                  "recipient_name": "Jane Foster",
+                                  "line1": "Travis County",
+                                  "line2": "",
+                                  "city": "Austin",
+                                  "country_code": "US",
+                                  "postal_code": "73301",
+                                  "phone": "+00000000",
+                                  "state": "Texas"
+                                },
                               }
-                            ],
-                            note: "Contact us for any questions on your order.",
-                            onSuccess: (Map params) async {
-                              print("onSuccess: $params");
-                            },
-                            onError: (error) {
-                              print("onError: $error");
-                            },
-                            onCancel: (params) {
-                              print('cancelled: $params');
-                            }),
-                      ),
-                    );
-                  
-            }, child: Text('Pay With PayPal'))
+                            }
+                          ],
+                          note: "Contact us for any questions on your order.",
+                          onSuccess: (Map params) async {
+                            print("onSuccess: $params");
+                            UIHelper.showAlertDialog('Payment Successfully',
+                                title: 'Success');
+                          },
+                          onError: (error) {
+                            print("onError: $error");
+                            UIHelper.showAlertDialog(
+                                'Unable to completet the Payment',
+                                title: 'Error');
+                          },
+                          onCancel: (params) {
+                            print('cancelled: $params');
+                            UIHelper.showAlertDialog('Payment Cannceled',
+                                title: 'Cancel');
+                          }),
+                    ),
+                  );
+                },
+                child: Text('Pay With PayPal'))
           ],
         ),
       ),
-      
     );
   }
 }
